e076473a5e3cd9387b640e0a1fc05442d10e2a57
Shimaa Saad <shimaasaad35790@gmail.com>
2025-04-16 01:48:28 +0200

    new Departments names

--- read_department.py
+++ read_department.py
@@ -2,9 +2,9 @@ import pandas as pd
 
 #  from intents_to_departments
 df = pd.read_csv("intents_to_departments.csv")
-print(df.iloc[:,0:2])
+print(df.iloc[:,1:4])
 
 print("\n")
 
 df2 = pd.read_csv("categories_to_departments.csv")
-print(df2.iloc[:,0:2])
+print(df2.iloc[:,1:4])
